4438bc14898750d8deb0af9ce8bc274716b09732
ryandam <ryandam.9@gmail.com>
2024-01-14 04:52:19 +1100

    05 - Multi step form

--- 05-multi-step-form/js/app.js
+++ 05-multi-step-form/js/app.js
@@ -55,7 +55,9 @@ window.onload = function () {
 let selectedPlan = "arcade";
 let selectedBillingType = "monthly";
 
+// -----------------------------------
 // Event listener for the next button
+// -----------------------------------
 document.querySelectorAll(".next-btn").forEach((button) => {
   button.addEventListener("click", function (e) {
     e.preventDefault();
@@ -80,7 +82,9 @@ document.querySelectorAll(".next-btn").forEach((button) => {
   });
 });
 
+// -----------------------------------
 // Event listener for the previous button
+// -----------------------------------
 document.querySelectorAll(".prev-btn").forEach((button) => {
   button.addEventListener("click", function (e) {
     e.preventDefault();
@@ -92,10 +96,10 @@ document.querySelectorAll(".prev-btn").forEach((button) => {
   });
 });
 
+// -----------------------------------
 // Show and hide sections
+// -----------------------------------
 function showAndHideSection(currentStep, nextStep) {
-  console.log(currentStep, nextStep);
-
   document
     .getElementById(`step-${currentStep}-container`)
     .classList.add("hidden");
@@ -104,7 +108,9 @@ function showAndHideSection(currentStep, nextStep) {
     .classList.remove("hidden");
 }
 
+// -----------------------------------
 // Highlight circle
+// -----------------------------------
 function highlightCircle(nextStep) {
   // Remove active class from all circles
   document.querySelectorAll(".step-number").forEach((step) => {
@@ -119,8 +125,95 @@ function highlightCircle(nextStep) {
   });
 }
 
+// -----------------------------------
+// Step #1
+// Form validation
+// -----------------------------------
+function validateFormFields() {
+  // Input elements
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let phoneNumber = document.getElementById("phone-number");
+
+  // Error message elements
+  let nameMsg = document.getElementById("name-msg");
+  let emailMsg = document.getElementById("email-msg");
+  let phoneNumberMsg = document.getElementById("phone-number-msg");
+
+  // User entered values.
+  let nameInput = name.value.trim();
+  let emailInput = email.value.trim();
+  let phoneNumberInput = phoneNumber.value.trim();
+
+  let validationPassed = true;
+
+  // When validation starts, remove error messages.
+  name.classList.remove("error");
+  email.classList.remove("error");
+  phoneNumber.classList.remove("error");
+  nameMsg.textContent = "";
+  emailMsg.textContent = "";
+  phoneNumberMsg.textContent = "";
+
+  if (nameInput === "") {
+    name.classList.add("error");
+    nameMsg.textContent = "This field is required";
+    validationPassed = false;
+  }
+
+  if (emailInput === "") {
+    email.classList.add("error");
+    emailMsg.textContent = "This field is required";
+    validationPassed = false;
+  }
+
+  // Check if email is valid
+  if (emailInput !== "") {
+    let emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(emailInput)) {
+      email.classList.add("error");
+      emailMsg.textContent = "Please enter a valid email";
+      validationPassed = false;
+    }
+  }
+
+  if (phoneNumberInput === "") {
+    phoneNumber.classList.add("error");
+    phoneNumberMsg.textContent = "This field is required";
+    validationPassed = false;
+  }
+
+  // If phone number contains non-numeric characters except +
+  // then it is invalid.
+  if (phoneNumberInput !== "") {
+    let phoneNumberRegex = /^[0-9+]+$/;
+    if (!phoneNumberRegex.test(phoneNumberInput)) {
+      phoneNumberMsg.textContent = "Please enter a valid phone number";
+      validationPassed = false;
+    }
+  }
+
+  return validationPassed;
+}
+
+// -----------------------------------
+// Step #1
+// When the user focuses on a form field, remove the error message
+// -----------------------------------
+document.querySelectorAll(".form-field").forEach((field) => {
+  field.addEventListener("focus", function (e) {
+    // Remove border.
+    e.target.classList.remove("error");
+
+    let msg = e.target.parentNode.querySelector(".error-msg");
+    msg.textContent = "";
+  });
+});
+
+// -----------------------------------
 // Step #2
 // When plan type is changed, update the price and freebie
+// -----------------------------------
 document
   .getElementById("plan-checkbox")
   .addEventListener("change", function (e) {
@@ -167,9 +260,11 @@ document
     updateAddonServices();
   });
 
+// -----------------------------------
 // Step #2
 //   Highlight selected plan
 //   This is done by adding the "selected" class to the plan
+// -----------------------------------
 document
   .querySelector(".plans")
   .querySelectorAll("li")
@@ -204,9 +299,10 @@ document
     });
   });
 
+// -----------------------------------
 // Step #3
 // Add ons
-
+// -----------------------------------
 document.querySelectorAll(".addon-item").forEach((addon) => {
   // Identify the checkbox inside it.
   const checkbox = addon.querySelector("input[type=checkbox]");
@@ -225,8 +321,116 @@ document.querySelectorAll(".addon-item").forEach((addon) => {
   });
 });
 
+// -----------------------------------
+// Generate Add on services
+// -----------------------------------
+function generateAddonServices() {
+  for (let itemId in addonServices) {
+    const item = addonServices[itemId];
+    let listElement = document.createElement("li");
+    listElement.className = "addon-item-container";
+
+    let div1 = document.createElement("div");
+    div1.className = "addon-item";
+
+    // Checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = item.name.replace(/ /g, "-").toLowerCase() + "-addon";
+
+    // Add event listener to the checkbox
+    checkbox.addEventListener("change", function (e) {
+      // If checkbox is selected, add a border to its parent's parent
+      // Otherwise, remove the border
+      if (e.target.checked) {
+        listElement.classList.add("checked");
+      } else {
+        listElement.classList.remove("checked");
+      }
+    });
+
+    // Name & Description
+    let div2 = document.createElement("div");
+
+    let p1 = document.createElement("p");
+    p1.className = "addon-item-type";
+    p1.textContent = item.name;
+
+    let p2 = document.createElement("p");
+    p2.className = "addon-item-summary";
+    p2.textContent = item.description;
+
+    div2.appendChild(p1);
+    div2.appendChild(p2);
+
+    // Price
+    let p3 = document.createElement("p");
+    p3.className = "addon-item-price";
+
+    if (selectedBillingType === "monthly") {
+      p3.textContent = `+$${item.price}/mo`;
+    } else {
+      p3.textContent = `+$${item.price * 10}/yr`;
+    }
+
+    div1.appendChild(checkbox);
+    div1.appendChild(div2);
+
+    listElement.appendChild(div1);
+    listElement.appendChild(p3);
+
+    document.querySelector(".addon-items-list").appendChild(listElement);
+
+    // When anywhere in "div1" item is clicked, toggle the checkbox
+    listElement.addEventListener("click", function (e) {
+      // If the clicked element is the checkbox, do nothing
+      if (e.target === checkbox) {
+        return;
+      }
+
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event("change"));
+    });
+  }
+}
+
+// -----------------------------------
+// This is called when the user changes the billing type
+// between monthly and yearly.
+// It will update the prices of the addons.
+// -----------------------------------
+function updateAddonServices() {
+  document.querySelectorAll(".addon-item-container").forEach((addon) => {
+    // Identify the checkbox inside it.
+    const checkbox = addon.querySelector("input[type=checkbox]");
+
+    // Update price
+    let price = addon.querySelector(".addon-item-price");
+
+    if (selectedBillingType === "monthly") {
+      price.textContent = `+$${addonPrices[checkbox.id]}/mo`;
+    } else {
+      price.textContent = `+$${addonPrices[checkbox.id] * 10}/yr`;
+    }
+  });
+}
+
+document
+  .getElementById("summary-change-link")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Go back to step 1
+    showAndHideSection(4, 2);
+
+    // Highlight the first circle
+    highlightCircle(2);
+  });
+
+// -----------------------------------
 // Step #5
 // Summary
+// -----------------------------------
 function calculateFinalPrice() {
   let finalPrice = 0;
 
@@ -297,175 +501,3 @@ function calculateFinalPrice() {
   document.querySelector(".total-price-amt").innerText =
     `+$${finalPrice}${billingType}`;
 }
-
-function generateAddonServices() {
-  for (let itemId in addonServices) {
-    const item = addonServices[itemId];
-    let listElement = document.createElement("li");
-    listElement.className = "addon-item-container";
-
-    let div1 = document.createElement("div");
-    div1.className = "addon-item";
-
-    // Checkbox
-    let checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.id = item.name.replace(/ /g, "-").toLowerCase() + "-addon";
-
-    // Add event listener to the checkbox
-    checkbox.addEventListener("change", function (e) {
-      // If checkbox is selected, add a border to its parent's parent
-      // Otherwise, remove the border
-      if (e.target.checked) {
-        listElement.classList.add("checked");
-      } else {
-        listElement.classList.remove("checked");
-      }
-    });
-
-    // Name & Description
-    let div2 = document.createElement("div");
-
-    let p1 = document.createElement("p");
-    p1.className = "addon-item-type";
-    p1.textContent = item.name;
-
-    let p2 = document.createElement("p");
-    p2.className = "addon-item-summary";
-    p2.textContent = item.description;
-
-    div2.appendChild(p1);
-    div2.appendChild(p2);
-
-    // Price
-    let p3 = document.createElement("p");
-    p3.className = "addon-item-price";
-
-    if (selectedBillingType === "monthly") {
-      p3.textContent = `+$${item.price}/mo`;
-    } else {
-      p3.textContent = `+$${item.price * 10}/yr`;
-    }
-
-    div1.appendChild(checkbox);
-    div1.appendChild(div2);
-
-    listElement.appendChild(div1);
-    listElement.appendChild(p3);
-
-    document.querySelector(".addon-items-list").appendChild(listElement);
-  }
-}
-
-// This is called when the user changes the billing type
-// between monthly and yearly.
-// It will update the prices of the addons.
-function updateAddonServices() {
-  document.querySelectorAll(".addon-item-container").forEach((addon) => {
-    // Identify the checkbox inside it.
-    const checkbox = addon.querySelector("input[type=checkbox]");
-
-    // Update price
-    let price = addon.querySelector(".addon-item-price");
-    console.log(price);
-
-    if (selectedBillingType === "monthly") {
-      price.textContent = `+$${addonPrices[checkbox.id]}/mo`;
-    } else {
-      price.textContent = `+$${addonPrices[checkbox.id] * 10}/yr`;
-    }
-  });
-}
-
-document
-  .getElementById("summary-change-link")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-
-    // Go back to step 1
-    showAndHideSection(4, 2);
-
-    // Highlight the first circle
-    highlightCircle(2);
-  });
-
-// Step #1
-// Form validation
-function validateFormFields() {
-  // Input elements
-  let name = document.getElementById("name");
-  let email = document.getElementById("email");
-  let phoneNumber = document.getElementById("phone-number");
-
-  // Error message elements
-  let nameMsg = document.getElementById("name-msg");
-  let emailMsg = document.getElementById("email-msg");
-  let phoneNumberMsg = document.getElementById("phone-number-msg");
-
-  // User entered values.
-  let nameInput = name.value.trim();
-  let emailInput = email.value.trim();
-  let phoneNumberInput = phoneNumber.value.trim();
-
-  let validationPassed = true;
-
-  // When validation starts, remove error messages.
-  name.classList.remove("error");
-  email.classList.remove("error");
-  phoneNumber.classList.remove("error");
-  nameMsg.textContent = "";
-  emailMsg.textContent = "";
-  phoneNumberMsg.textContent = "";
-
-  if (nameInput === "") {
-    name.classList.add("error");
-    nameMsg.textContent = "This field is required";
-    validationPassed = false;
-  }
-
-  if (emailInput === "") {
-    email.classList.add("error");
-    emailMsg.textContent = "This field is required";
-    validationPassed = false;
-  }
-
-  // Check if email is valid
-  if (emailInput !== "") {
-    let emailRegex = /\S+@\S+\.\S+/;
-    if (!emailRegex.test(emailInput)) {
-      email.classList.add("error");
-      emailMsg.textContent = "Please enter a valid email";
-      validationPassed = false;
-    }
-  }
-
-  if (phoneNumberInput === "") {
-    phoneNumber.classList.add("error");
-    phoneNumberMsg.textContent = "This field is required";
-    validationPassed = false;
-  }
-
-  // If phone number contains non-numeric characters except +
-  // then it is invalid.
-  if (phoneNumberInput !== "") {
-    let phoneNumberRegex = /^[0-9+]+$/;
-    if (!phoneNumberRegex.test(phoneNumberInput)) {
-      phoneNumberMsg.textContent = "Please enter a valid phone number";
-      validationPassed = false;
-    }
-  }
-
-  return validationPassed;
-}
-
-// Step #1
-// When the user focuses on a form field, remove the error message
-document.querySelectorAll(".form-field").forEach((field) => {
-  field.addEventListener("focus", function (e) {
-    // Remove border.
-    e.target.classList.remove("error");
-
-    let msg = e.target.parentNode.querySelector(".error-msg");
-    msg.textContent = "";
-  });
-});
