b4396eb29f65eab007528187446e89acecba4d37
RK <ryandam.9@gmail.com>
2024-02-14 07:46:13 +1100

    6 - news page

--- 06-news-homepage/app.js
+++ 06-news-homepage/app.js
@@ -4,5 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   menuIcon.addEventListener("click", function () {
     mobileMenu.classList.toggle("show");
+
+    // Hide the hamburger menu when
+    menuIcon.classList.toggle("hide");
   });
 });
+
+// Close the mobile menu when the user clicks outside of it
+document.addEventListener("click", function (event) {
+  let mobileMenu = document.getElementById("mobile-menu-wrapper");
+  let menuIcon = document.querySelector("#mobile-nav img.menu-icon");
+
+  if (event.target !== mobileMenu && event.target !== menuIcon) {
+    mobileMenu.classList.remove("show");
+    menuIcon.classList.remove("hide");
+  }
+});
+
+// Close the mobile menu when clicked on Close button
+document
+  .querySelector(".menu-close-icon")
+  .addEventListener("click", function () {
+    document.getElementById("mobile-menu-wrapper").classList.remove("show");
+    document
+      .querySelector("#mobile-nav img.menu-icon")
+      .classList.remove("hide");
+  });
